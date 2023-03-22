bf952530df715e645dd8506bba7b2c89c75d72d4
DevAkky <developerakky@gmail.com>
2023-03-22 20:31:32 +0530

    tailwind config done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
-
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
