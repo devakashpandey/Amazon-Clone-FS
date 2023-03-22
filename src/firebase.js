e0ebf00e84f12a071b50081e6ec9aa4fb2347416
DevAkky <developerakky@gmail.com>
2023-03-22 17:52:52 +0530

    header start

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsMbAfbb8T63Wm_7I1TAfyhJqT2zxrX24",
+  authDomain: "challenge-24dbf.firebaseapp.com",
+  projectId: "challenge-24dbf",
+  storageBucket: "challenge-24dbf.appspot.com",
+  messagingSenderId: "110609250052",
+  appId: "1:110609250052:web:60489347100c4efcbdc251",
+};
+
+const app = initializeApp(firebaseConfig);
