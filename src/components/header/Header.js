213ecfc4e472275fe6f1cc7574d0c37da769d290
DevAkky <developerakky@gmail.com>
2023-03-22 20:46:29 +0530

    assets added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,35 +4,10 @@ import "./Header.css";
 const Header = () => {
   return (
     <>
-      <div className="header">
-        <img
-          className="header-logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        ></img>
-        <div className="header-search">
-          <input
-            type="text"
-            className="search-input"
-            placeholder="Search Amazon.in"
-          />
-        </div>
-        <div className="header-nav">
-          <div className="header-option">
-            <span className="option-1">🇮🇳</span>
-          </div>
-          <div className="header-option">
-            <span className="option-2">Hello, sign in</span>
-            <span className="option-2">Accounts & Lists</span>
-          </div>
-          <div className="header-option">
-            <span className="option-3">Returns</span>
-            <span className="option-3">& Orders</span>
-          </div>
-          <div className="header-option">
-            <span className="option-4">Hello, sign in</span>
-            <span className="option-4">Accounts & Lists</span>
-          </div>
-        </div>
+      <div className="w-full bg-amazon_blue text-white px-4 py-4">
+        {/* ========== Image Start here ========== */}
+
+        {/* ========== Image End here ========== */}
       </div>
     </>
   );
