2e7639a0ecd583ab9155561427875a633ce92a3d
DevAkky <developerakky@gmail.com>
2023-03-31 20:20:31 +0530

    footer top designing

--- src/components/footer/FooterTop.js
+++ src/components/footer/FooterTop.js
@@ -3,7 +3,22 @@ import React from "react";
 const FooterTop = () => {
   return (
     <>
-      <div></div>
+      <div className="w-full bg-white py-6">
+        <div className="w-full border-t-[1px] border-b-[1px] py-8">
+          <div className="w-64 mx-auto text-center">
+            <p className="text-sm py-1">See personalized recommendations</p>
+            <button
+              className="w-full bg-yellow-400 rounded-md py-1 text-sm font-semibold 
+              hover:bg-yellow-500 cursor-pointer"
+            >
+              Sign in
+            </button>
+            <p className="text-xs mt-1">
+              New Customer? <span>Start here</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
