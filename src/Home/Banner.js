515eeb4f9cb927f2c9dfbdac7199e1c460c07619
DevAkky <developerakky@gmail.com>
2023-03-31 17:28:03 +0530

    caraousel done

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Banner.css";
 import Slider from "react-slick";
 import banner1 from "../assets/banner/banner1.jpg";
 import banner2 from "../assets/banner/banner2.jpg";
@@ -7,6 +8,7 @@ import banner4 from "../assets/banner/banner4.jpg";
 import banner5 from "../assets/banner/banner5.jpg";
 
 const Banner = () => {
+  const [dotActive, setDotActive] = useState(0);
   // react slick caraousel setting
   const settings = {
     dots: true,
@@ -16,28 +18,61 @@ const Banner = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
+    beforeChange: (prev, next) => {
+      setDotActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          position: "absolute",
+          top: "70%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "15rem",
+        }}
+      >
+        <ul
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          {dots}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div className={i === dotActive ? "pagination" : "pagination2"}>
+        {i + 1}
+      </div>
+    ),
   };
 
   return (
     <>
-      <div>
-        <Slider {...settings}>
-          <div>
-            <img src={banner1} alt="banner" />
-          </div>
-          <div>
-            <img src={banner2} alt="banner" />
-          </div>
-          <div>
-            <img src={banner3} alt="banner" />
-          </div>
-          <div>
-            <img src={banner4} alt="banner" />
-          </div>
-          <div>
-            <img src={banner5} alt="banner" />
-          </div>
-        </Slider>
+      <div className="w-full">
+        <div className="w-full h-full relative">
+          <Slider {...settings} className="slider">
+            <div>
+              <img src={banner1} alt="banner" />
+            </div>
+            <div>
+              <img src={banner2} alt="banner" />
+            </div>
+            <div>
+              <img src={banner3} alt="banner" />
+            </div>
+            <div>
+              <img src={banner4} alt="banner" />
+            </div>
+            <div>
+              <img src={banner5} alt="banner" />
+            </div>
+          </Slider>
+        </div>
       </div>
     </>
   );
