264f455f70c2df19d666b3ad554c78e1b07cad25
DevAkky <developerakky@gmail.com>
2023-04-02 11:34:41 +0530

    footer midle links styling

--- src/components/footer/FooterMiddle.js
+++ src/components/footer/FooterMiddle.js
@@ -3,7 +3,33 @@ import React from "react";
 const FooterMiddle = () => {
   return (
     <>
-      <div></div>
+      <div className="w-full bg-amazon_light text-white">
+        <div
+          className="w-full bg-[#37475a] py-3 text-center text-sm 
+        hover:bg-[#37476b] cursor-pointer"
+        >
+          Back to top
+        </div>
+
+        <div className="w-full py-10">
+          <div className="max-w-5xl mx-auto text-gray-300">
+            <div>
+              <h3 className="font-titleFont text-white font-semibold mb-3">
+                Get to Know Us
+              </h3>
+              <ul className="flex flex-col gap-1 font-bodyFont text-sm">
+                <li className="footerLinkHover">Careers</li>
+                <li className="footerLinkHover">Blog</li>
+                <li className="footerLinkHover">About Amazon</li>
+                <li className="footerLinkHover">Investor Relation</li>
+                <li className="footerLinkHover">Amazon Devices</li>
+                <li className="footerLinkHover">Amazon Science</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </>
   );
 };
--- src/components/footer/FooterTop.js
+++ src/components/footer/FooterTop.js
@@ -4,17 +4,18 @@ const FooterTop = () => {
   return (
     <>
       <div className="w-full bg-white py-6">
-        <div className="w-full border-t-[1px] border-b-[1px] py-8">
+        <div className="w-full border-t-[1px] border-b-[1px] py-6">
           <div className="w-64 mx-auto text-center">
             <p className="text-sm py-1">See personalized recommendations</p>
             <button
-              className="w-full bg-yellow-400 rounded-md py-1 text-sm font-semibold 
+              className="w-full bg-yellow-400 rounded-md py-2 text-sm font-semibold 
               hover:bg-yellow-500 cursor-pointer"
             >
               Sign in
             </button>
-            <p className="text-xs mt-1">
-              New Customer? <span>Start here</span>
+            <p className="text-xs mt-2">
+              New customer?{" "}
+              <span className="text-blue-600 cursor-pointer">Start here</span>
             </p>
           </div>
         </div>
