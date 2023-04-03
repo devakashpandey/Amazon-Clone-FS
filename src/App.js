c502acbe6ad3bfd3dc7d8ba8b9e71cd9ee90c847
DevAkky <developerakky@gmail.com>
2023-04-03 11:10:10 +0530

    router setup done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Banner from "./Home/Banner";
 import Footer from "./components/footer/Footer";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route path="/" element={<Layout />} />)
+  );
+
   return (
     <div className="font-bodyFont">
-      <Header />
+      <RouterProvider router={router}></RouterProvider>
+      {/* <Header />
       <Banner />
-      <Footer />
+      <Footer /> */}
     </div>
   );
-}
+};
 
 export default App;
