2acb0afbd418236e765422394ef00e69de7bc000
DevAkky <developerakky@gmail.com>
2023-04-04 14:28:28 +0530

    product hover desiging

--- src/pages/home/products/Product.js
+++ src/pages/home/products/Product.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import StarIcon from "@mui/icons-material/Star";
 import StarHalfIcon from "@mui/icons-material/StarHalf";
+import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const Product = () => {
   const [myProductData, setMyProductData] = useState([]);
@@ -17,21 +19,41 @@ const Product = () => {
 
   return (
     <>
-      <div className="max-w-screen-3xl max-auto grid grid-cols-4 gap-8 px-5">
+      <div className="max-w-screen-3xl max-auto grid grid-cols-4 gap-8 px-6">
         {myProductData?.map((item) => (
           <div
-            className="bg-white h-auto border-[1px] py-6 hover:border-transparent
+            className="bg-white h-auto border-[1px] py-8 hover:border-transparent
             hover:shadow-testShadow duration-200 relative "
             key={item.id}
           >
             <div className="flex flex-col gap-4 cursor-pointer">
-              <div className="w-full h-auto flex items-center justify-center">
+              <div className="w-full h-auto flex items-center justify-center relative">
                 <img
                   className="w-52 h-64 object-contain"
                   src={item.image}
                   alt="productImage"
                 />
+                <ul
+                  className="w-full h-32 bg-gray-100 absolute bottom-0 
+                duration-700 flex flex-col items-center justify-center gap-4 font-titleFont px-2 border-1 border-r "
+                >
+                  <li className="productli">
+                    Compare{" "}
+                    <span>
+                      <CompareArrowsIcon />
+                    </span>
+                  </li>
+                  <li className="productli">
+                    Add to Favorite{" "}
+                    <span>
+                      <FavoriteIcon />
+                    </span>
+                  </li>
+                </ul>
               </div>
+              <span className="text-xs capitalize italic top-2 right-2 absolute text-gray-500">
+                {item.category}
+              </span>
               <div className="px-4">
                 <div className="flex items-center justify-between">
                   <h2 className="font-titleFont text-lg">
@@ -52,6 +74,14 @@ const Product = () => {
                     <span className="text-[#187096] text-sm"> (13,321)</span>
                   </div>
                 </div>
+                <button
+                  className="w-full font-titleFont bg-gradient-to-tr from-yellow-400
+                   to-yellow-200 border hover:from-yellow-300 hover:to-yellow-400
+                    border-yellow-400 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500
+                    duration-200 py-1 text-sm rounded-md mt-3"
+                >
+                  Add To Cart
+                </button>
               </div>
             </div>
           </div>
